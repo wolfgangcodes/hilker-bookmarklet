a342e9cc065b59b94cdb3373726fac0678e3575f
Wolfgang Therrien <wolfgang@hubspot.com>
2016-06-23 11:32:55 -0400

    fixit

--- lgtm.js
+++ lgtm.js
@@ -1,4 +1,4 @@
-function lgtm() {
+;(function lgtm() {
   document.activeElement.value = ">LGTM\n\n"+
-  "– Good Code Cat™ ![LGTM](https://emoji.slack-edge.com/T024WBZ4E/glasscat/1ba56bc1228df3cf.png)"
-}()
+  "– Good Code Cat™ ![LGTM](https://emoji.slack-edge.com/T024WBZ4E/glasscat/1ba56bc1228df3cf.png)";
+})();
